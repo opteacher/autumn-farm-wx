4d797eefcf5609ac37e1c43d65ffc0143a78ce2d
opower <george.zhao@viewfin.com>
2018-08-14 15:43:43 +0800

    Made home page

---
+++ src/layouts/main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <slot/>
+        <div class="row fixed-bottom p-2" id="btmNavBar">
+            <div class="col">
+                <button type="button" class="btn btn-success w-100">主页</button>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-success w-100">分类</button>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-success w-100">账户</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style type="text/scss">
+    #btmNavBar {
+        background-color: #eeeeee;
+    }
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>abcd</h1>
+    <main-layout>
+        <head-adv-bar/>
+    </main-layout>
 </template>
+
+<script>
+    import mainLayout from "../layouts/main"
+    import headAdvBar from "../components/hdAdvBar"
+
+    export default {
+        components: {
+            "main-layout": mainLayout,
+            "head-adv-bar": headAdvBar
+        }
+    }
+</script>
+
+<style>
+    .el-carousel {
+        padding-left: -15px;
+        padding-right: -15px;
+    }
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 14px;
+        opacity: 0.75;
+        line-height: 150px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+</style>
