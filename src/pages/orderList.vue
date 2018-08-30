702c47f8ccfbf205bff71c206316177221bc6d53
opower <george.zhao@viewfin.com>
2018-08-30 18:19:17 +0800

    Updated

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -26,7 +26,7 @@
                                     <i class="iconfont green-text" :class="iconMap[prods[order.prodId].type]"></i>&nbsp;{{prods[order.prodId].name}}
                                 </h4>
                                 <p class="weui-media-box__desc mb-2">{{prods[order.prodId].title}}</p>
-                                <div class="weui-media-box__desc weui-flex">
+                                <div class="weui-media-box__desc weui-flex w-100">
                                     <div class="weui-flex__item">
                                         <i class="iconfont icon-xiangxi green-text"></i>&nbsp;{{order.amount}}&nbsp;x&nbsp;{{order.unit}}{{order.expTyp ? ` + ${order.expTyp}` : ""}}
                                     </div>
